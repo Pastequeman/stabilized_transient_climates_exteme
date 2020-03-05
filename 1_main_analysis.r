d40cc285afa59164bcc1243a2076a0d58983d9f2
BOULANGE Julien Eric Stanislas <julien@pc202-101.nies.go.jp>
2020-03-05 18:13:20 +0900

    all scripts completed

--- 1_main_analysis.r
+++ 1_main_analysis.r
@@ -50,6 +50,7 @@ outputs <- list(max_dis_v1 = matrix(data = 0, nrow = 30, ncol = 259200),
                 p95_dis_v2 = matrix(data = 0, nrow = 30, ncol = 259200)
                 )
 #
+leap_years <- c(1664, 1668, 1672, 1676, 1680, 1684, 1688, 1692, 1696, 1704, 1708, 1712, 1716, 1720, 1724, 1728, 1732, 1736, 1740, 1744, 1748, 1752, 1756, 1760, 1764, 1768, 1772, 1776, 1780, 1784, 1788, 1792, 1796, 1804, 1808, 1812, 1816, 1820, 1824, 1828, 1832, 1836, 1840, 1844, 1848, 1852, 1856, 1860, 1864, 1868, 1872, 1876, 1880, 1884, 1888, 1892, 1896, 1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052, 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084, 2088, 2092, 2096, 2104, 2108, 2112, 2116, 2120, 2124, 2128, 2132, 2136, 2140, 2144, 2148, 2152, 2156, 2160, 2164, 2168, 2172, 2176, 2180, 2184, 2188, 2192, 2196, 2204, 2208, 2212, 2216, 2220, 2224, 2228, 2232, 2236, 2240, 2244, 2248, 2252, 2256, 2260, 2264, 2268, 2272, 2276, 2280, 2284, 2288, 2292, 2296)
 out_var <- c("max", "p05", "min", "p95", "max", "p05", "min", "p95")
 ## read the files
 PERIOD <- 0
@@ -115,11 +116,19 @@ for (EXP in EXPERIMENTS) {
       start_y <- 1
     } else {
       start_y <- seq(as.Date(paste0(ini, "/1/1")), as.Date(paste((years - 1), "/12/31", sep = "")), "day")
+      if (MODEL %in% c("CLM45", "lpjml")) {
+        start_y <- start_y[!(format(start_y,"%m") == "02" & format(start_y, "%d") == "29")]
+      }
     }
 
 # 
-    # start :   ; 
-    discharge <- ncvar_get(file_1, "dis", start = c(1, 1, length(start_y)), count = c(-1, -1, length(days)))
+    # start :   ;
+    if (MODEL %in% c("CLM45", "lpjml") & years %in% leap_years) {
+      adj <- -1
+    } else {
+      adj <- 0
+    }
+    discharge <- ncvar_get(file_1, "dis", start = c(1, 1, length(start_y)), count = c(-1, -1, (length(days)+adj)))
 
     nc_close(file_1)
     # 4 indices (the order of the data is correct! no need to transpose here)
@@ -176,5 +185,4 @@ for (i in 1:8) {
                    EXPERIMENTS[2], "_", as.character(PERIOD_2[1]), "-", as.character(PERIOD_2[2]), "_",
                    out_var[i], ".csv"))  
   }
-
 }
--- 2_summarise.r
+++ 2_summarise.r
@@ -10,15 +10,15 @@ args = commandArgs(trailingOnly = TRUE)
 
 
 ## Libraries
-library("ncdf4")
+#library("ncdf4")
 library("readr")
 suppressMessages(library("dplyr"))
+library("ggplot2")
 
 ## read files
-
-for (m in c("cwatm_", "matsiro_", "clm45_", "lpjml", "pcr-globwb", "watergap2", "h08")) { # add more as they come
+for (m in c("cwatm_", "matsiro_", "clm45_", "lpjml_", "pcr-globwb_", "watergap2_", "h08_")) { # add more as they come
   for (gcms in c("hadgem2-es_", "ipsl-cm5a-lr_", "miroc5_")) {
-    if ((m == "clm45_" & gcms %in% c("ipsl-cm5a-lr_", "miroc5_")) | (m == "h08" & gcms == "miroc5_")) {
+    if ((m == "clm45_" & gcms %in% c("ipsl-cm5a-lr_", "miroc5_")) | (m == "h08_" & gcms == "miroc5_")) {
       next
     }
     if (gcms == "hadgem2-es_") {
@@ -39,7 +39,7 @@ for (m in c("cwatm_", "matsiro_", "clm45_", "lpjml", "pcr-globwb", "watergap2", 
       temp$model  <- m
       temp$gcm    <- gcms
 
-      if (gcms == "hadgem2-es_" & m == "cwatm_") {
+      if (gcms == "hadgem2-es_" & m == "cwatm_" & id == "max") {
         agg_data <- temp
       } else {
         agg_data <- rbind(agg_data, temp)
@@ -56,7 +56,7 @@ non_agg <-
 agg_data %>% group_by(model, gcm, indice) %>% summarise(n = n(),
                                                         n_pos = sum(to_save >= 0.05, na.rm = TRUE),
                                                         n_na = sum(is.na(to_save)))
-non_agg$n_neg <- (non_agg$n - non_agg$n_na) - non_agg$n_pos
+non_agg$n_neg    <- (non_agg$n - non_agg$n_na) - non_agg$n_pos
 non_agg$cen_neg  <- non_agg$n_neg / (non_agg$n - non_agg$n_na) * 100
 non_agg$cen_pos  <- non_agg$n_pos / (non_agg$n - non_agg$n_na) * 100
 non_agg
@@ -64,16 +64,49 @@ non_agg
 write_csv(non_agg, "./../OUT/trans_vs_stab_non_aggregated.csv")
 
 ### aggregate
-non_agg %>% group_by(indice) %>% summarise(ave_cen_neg = mean(cen_neg, na.rm = TRUE),
-                                          ave_cen_pos = mean(cen_pos, na.rm = TRUE))
+non_agg %>% group_by(indice) %>% summarise(ave_cen_neg  = mean(cen_neg, na.rm = TRUE),
+                                           ave_cen_pos = mean(cen_pos, na.rm = TRUE),
+                                           sd_cen_neg  = sd(cen_neg, na.rm = TRUE),
+                                           sd_cen_pos  = sd(cen_pos, na.rm = TRUE))
 
+# model based
+agg_model <- 
+non_agg %>% group_by(indice, model) %>% summarise(ave_cen_neg  = mean(cen_neg, na.rm = TRUE),
+                                           ave_cen_pos = mean(cen_pos, na.rm = TRUE),
+                                           sd_cen_neg  = sd(cen_neg, na.rm = TRUE),
+                                           sd_cen_pos  = sd(cen_pos, na.rm = TRUE))
 
-
+write_csv(agg_model, "./../OUT/trans_vs_stab_only_model.csv")
 
 #### spatial distribution
 ## two things to look at:
 ## 1) aggrement within the ensemble
 ## 2) mean or median change of the ensemble between transient and stabiliazed
-
+grid_cell <-
 agg_data %>% group_by(id, indice) %>% summarise(n = n(),
-                                               agreement = sum())
+                                                n_sup = sum(to_save >= 0.05, na.rm = TRUE),
+                                                n_na = sum(is.na(to_save)),
+                                                n_neg = sum(to_save < 0.05, na.rm = TRUE))
+grid_cell
+## test
+a <- grid_cell %>% filter(indice == "max")
+
+a$lon <- rep(seq(-179.75, by = 0.5, length.out = 720), times = 360)
+a$lat <- rep(seq(89.75, by = -0.5, length.out = 360), each = 720)
+
+g <- ggplot(a %>% filter(n_na != 18))
+g + geom_tile(aes(x = lon, y = lat, fill = n_neg))
+
+# save independaently
+write_csv(a, "./../OUT/grid_cells_max.csv")
+a <- grid_cell %>% filter(indice == "min")
+a$lon <- rep(seq(-179.75, by = 0.5, length.out = 720), times = 360) ; a$lat <- rep(seq(89.75, by = -0.5, length.out = 360), each = 720)
+write_csv(a, "./../OUT/grid_cells_min.csv")
+
+a <- grid_cell %>% filter(indice == "p05")
+a$lon <- rep(seq(-179.75, by = 0.5, length.out = 720), times = 360) ; a$lat <- rep(seq(89.75, by = -0.5, length.out = 360), each = 720)
+write_csv(a, "./../OUT/grid_cells_p05.csv")
+
+a <- grid_cell %>% filter(indice == "p95")
+a$lon <- rep(seq(-179.75, by = 0.5, length.out = 720), times = 360) ; a$lat <- rep(seq(89.75, by = -0.5, length.out = 360), each = 720)
+write_csv(a, "./../OUT/grid_cells_p95.csv")
